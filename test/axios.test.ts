88a87c4e79fb3ac6629699e6128cddc53ea302ec
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-05-13 10:10:05 +0800

    chore: emm

--- test/axios.test.ts
+++ test/axios.test.ts
@@ -2,15 +2,13 @@
 import { describe, it, expect, vi } from 'vitest'
 import axios from 'axios'
 
+vi.mock('axios');
+
 function buyApples() {
   return axios('https://www.fastmock.site/mock/6f92f55a6b8b6a1bdf0a2f35dcbe01ec/data1/1')
 }
 
 
 it('axios', async () => {
-  // toEqual returns a promise now, so you HAVE to await it
   const res = await buyApples()
-  expect(res.data).toMatchSnapshot()
-  // await expect(buyApples()).resolves.toMatchSnapshot() // jest API
-  // await expect(buyApples()).resolves.to.equal({ id: 1 }) // chai API
 })
